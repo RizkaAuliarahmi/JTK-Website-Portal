2e8c28e26c006b8fca285e70d4062602e1bc7596
Rizka <rizka.auliarahmi.tif419@polban.ac.id>
2022-05-28 15:44:36 +0700

    Update ProfilCard.vue

--- src/components/webjtk/ProfilCard.vue
+++ src/components/webjtk/ProfilCard.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
         <a  v-for="(data, index) in data" :key="index">
-            <div class="col-span-10 place-self-center mt-12">
+            <div class="col-span-10 place-self-center mt-12" v-if="index==0||index==2||index==4||index==18||index==12||index==7">
                 <span class="font-semibold font-montserrat text-2xl">
-                    {{judul}}
+                    {{judul[bagian_baru[index]]}}
                 </span>
             </div>
             <div :class="mulai_dari[index]" class="col-span-2 mt-4 back-color place-self-center">
@@ -22,22 +22,23 @@
 import Vue from "vue";
 export default Vue.extend({
   name: "ProfilCard",
-  props:["nama","nip","jabatan","gambar","mulai_dari"],
+  props:["data","index"],
+  //props:["nama","nip","jabatan","gambar","mulai_dari"],
 
    data() {
     return {
         judul:["Manajemen Jurusan Periode 2019-2023","Program Studi","Kelompok Bidang Keahlian (KBK)","Laboratorium","Wali kelas Program Studi Teknik Informatika D-III","Wali kelas Program Studi Teknik Informatika D-IV"],
         mulai_dari:["col-start-3","col-start-7", "col-start-3","col-start-7", "col-start-1", "col-start-5", "col-start-9", "col-start-3","col-start-7", "col-start-1", "col-start-5", "col-start-9", "col-start-1", "col-start-5", "col-start-9", "col-start-1", "col-start-5", "col-start-9", "col-start-3","col-start-7", "col-start-3","col-start-7"],
-        bagian_baru:[0,2,4,7,12,18]
+        bagian_baru:[0,1,1,1,2,2,2,3,3,3,3,3,4,4,4,4,4,4,5]
     };
   },
 
 mounted: {
     pickImage(){
-        if(data.gender == "male"){
-            gambar = "img/webjtk/Rectangle.png"
+        if(this.data.gender == "male"){
+            this.gambar = "img/webjtk/Rectangle.png"
         }else{
-            gambar = "img/webjtk/banner.png"
+            this.gambar = "img/webjtk/banner.png"
         }
     }
     
