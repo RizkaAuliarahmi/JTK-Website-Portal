5b6a09eb339fbf04cc05b2ce6d65b776356901ea
feuillelisse <nisashadrina@gmail.com>
2022-06-01 20:27:22 +0700

    Add partnership

--- src/views/Partnership.vue
+++ src/views/Partnership.vue
@@ -1,13 +1,33 @@
 <template>
-    <div class="partnership">
+    <div class="partnership font-montserrat">
         <Navbar/>
         <BannerImage
             title="Mitra Kami"
             image="http://www.hope-a.com/images/food/agency-partnership.jpg"
             color="text-white"
         />
-        <div>
-            
+        <div v-if="data" class="mt-10 mx-20">
+            <p class="text-sm color-gray"> Last Updated {{ data.last_update }} by {{ data.author }} </p>
+        </div>
+        <div class="mt-6 mx-20">
+            <div
+                v-for="(item, index) in data.content.description"
+                :key="index"
+                class="carousel-card mb-2 h-full"
+            >
+                <div class="text-justify text-sm">
+                    {{ data.content.description[index] }}
+                </div>
+                <br>
+            </div>
+        </div>
+        <div class="justify-center mx-5 mt-4">
+            <p class="text-m font-bold text-center"> Ayo bergabung dengan kami! </p>
+        </div>
+        <div class="place-items-center">
+           <div>
+                <v-btn color="orange" class="text-white">Gabung</v-btn>
+           </div>
         </div>
         <Footer/>
     </div>
@@ -23,11 +43,19 @@
 import BannerImage from "../components/webjtk/BannerImage.vue";
 import Navbar from "../components/webjtk/Navbar.vue";
 import Footer from "../components/webjtk/Footer.vue";
+import data from "@/dummy/partnership.json";
 
 export default {
     components: {
         BannerImage, Navbar, Footer
     },
     name: "Partnership",
+
+    data() {
+        return {
+            CMS_API: process.env.APP_CMS_API,
+            data: data,
+        };
+    },
 }
 </script>
