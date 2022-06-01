57b9f931f5a8dfef5c2aabd97cdb6834f6477fa0
friskaim7 <friska.intan2001@gmail.com>
2022-06-01 12:10:37 +0700

    fix: apply CSS rules for Event page

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -1,67 +1,93 @@
 <template>
   <div>
     <Navbar />
-    <img
-      src="../../public/img/events/layangan.png"
-      :style="{
-        background: 'url(' + image + ')',
-        'background-size': '100% 100%',
-      }"
-      class="grid grid-cols-2 w-screen h-60"
-    />
-    <div class="grid grid-cols-2 gap-4">
-      <h1>
-        SEMINAR INDUSTRI : PROGRAM MAGISTER TERAPAN REKAYASA INFRASTRUKTUR
-      </h1>
-      <button>Register</button>
-    </div>
-    <div>
-      <div>
-        <h2>Tempat</h2>
-        <p>Zoom Meeting</p>
+    <BannerImage :image="ChefImage" />
+    <div class="event-page-content">
+      <v-row>
+        <h1>
+          <strong>
+            SEMINAR INDUSTRI : PROGRAM MAGISTER TERAPAN REKAYASA INFRASTRUKTUR
+          </strong>
+        </h1>
+        <v-spacer></v-spacer>
+        <v-btn color="orange">Register</v-btn>
+      </v-row>
+      <div class="event-info">
+        <div>
+          <h2><strong> Tempat </strong></h2>
+          <p>Zoom Meeting</p>
+        </div>
+        <div>
+          <h2><strong> Waktu </strong></h2>
+          <p>14 April 2022</p>
+          <p>09.00 - 11.00 WIB</p>
+        </div>
+        <div>
+          <h2><strong> Pemateri </strong></h2>
+          <li>Panji Marshando</li>
+          <li>Lintang Dian Artanti</li>
+          <li>Azhar Lazuardiansyah</li>
+          <li>M. Taufik Habibilah Ibnu</li>
+        </div>
       </div>
-      <div>
-        <h2>Waktu</h2>
-        <p>14 April 2022</p>
-        <p>09.00 - 11.00 WIB</p>
+      <div class="grid grid-cols-2 gap-10">
+        <img src="../../public/img/events/layangan.png" />
+        <div class="event-desc">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi
+          tristique senectus et netus et malesuada fames ac. Suspendisse
+          interdum consectetur libero id faucibus nisl tincidunt. Tincidunt nunc
+          pulvinar sapien et ligula ullamcorper malesuada proin. Placerat
+          vestibulum lectus mauris ultrices. Orci eu lobortis elementum nibh
+          tellus molestie nunc. Ut ornare lectus sit amet est. Commodo quis
+          imperdiet massa tincidunt nunc. Orci sagittis eu volutpat odio
+          facilisis. Donec massa sapien faucibus et molestie ac feugiat.
+          Tincidunt vitae semper quis lectus nulla. Tellus pellentesque eu
+          tincidunt tortor aliquam. Non diam phasellus vestibulum lorem sed
+          risus ultricies. Quis blandit turpis cursus in hac habitasse. Suscipit
+          tellus mauris a diam maecenas sed enim ut.>
+        </div>
       </div>
-      <div>
-        <h2>Pemateri</h2>
-        <li>Panji Marshando</li>
-        <li>Lintang Dian Artanti</li>
-        <li>Azhar Lazuardiansyah</li>
-        <li>M. Taufik Habibilah Ibnu</li>
-      </div>
-    </div>
-    <div class="grid grid-cols-2 gap-4">
-      <img src="../../public/img/events/layangan.png" />
-      <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tristique senectus et netus et malesuada fames ac. Suspendisse interdum consectetur libero id faucibus nisl tincidunt. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin. Placerat vestibulum lectus mauris ultrices. Orci eu lobortis elementum nibh tellus molestie nunc. Ut ornare lectus sit amet est. Commodo quis imperdiet massa tincidunt nunc. Orci sagittis eu volutpat odio facilisis. Donec massa sapien faucibus et molestie ac feugiat. Tincidunt vitae semper quis lectus nulla. Tellus pellentesque eu tincidunt tortor aliquam. Non diam phasellus vestibulum lorem sed risus ultricies. Quis blandit turpis cursus in hac habitasse. Suscipit tellus mauris a diam maecenas sed enim ut.></div>
     </div>
     <!-- <EventCarousel /> -->
     <Footer />
   </div>
 </template>
 
-<script lang="ts">
+<script>
 import Vue from "vue";
-import Navbar from "./../components/webjtk/Navbar.vue"
-import Footer from "./../components/webjtk/Footer.vue"
+import BannerImage from "../components/webjtk/BannerImage.vue";
+import Navbar from "./../components/webjtk/Navbar.vue";
+import Footer from "./../components/webjtk/Footer.vue";
+import ChefImage from "./../../public/img/events/chef.png";
 
 export default Vue.extend({
   name: "Event",
   components: {
     Navbar,
+    BannerImage,
     // EventCarousel,
     Footer,
   },
-  //   data() {
-  //     return {
-  //       title: "Understanding Props in vuejs",
-  //       image: "../../public/img/events/layangan.png",
-  //     };
-  //   },
+  data: function () {
+    return {
+      title: "<Event Title>",
+      image: ChefImage,
+    };
+  },
 });
 </script>
 
 <style scoped>
+.event-info {
+  margin: 25px 0px 25px 0px;
+  text-align: center;
+  background-color: orange;
+}
+.event-page-content {
+  margin: 100px;
+}
+.event-desc {
+  text-align: justify;
+}
 </style>
