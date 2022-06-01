072778c76f6dbad80ec5ec664577eef4f99b51cd
feuillelisse <nisashadrina@gmail.com>
2022-06-01 15:13:35 +0700

    Adding news portal

--- src/components/webjtk/News/NewsPortraitCard.vue
+++ src/components/webjtk/News/NewsPortraitCard.vue
@@ -1,19 +1,40 @@
 <template>
     <!-- Image, title, date, & category -->
     <div class="flex w-full">
-      <div
-        v-for="(item, index) in data"
-        :key="index"
-        class="carousel-card mx-3 h-full"
-      >
-        <div>
-          <img
-            :src="item.img"
-            class="w-full h-max bg-no-repeat bg-cover rounded-t"
-          />
-          <div class="m4">
-            <p class="text-sm color-gray mb4">{{ item.date }} | {{ item.category }}</p>
-            <p class="text-xl mb-0">{{ item.title }}</p>
+      <div v-if="isIndex === false">
+        <div
+          v-for="(item, index) in data"
+          :key="index"
+          class="carousel-card mx-3 h-full"
+        >
+          <div>
+            <img
+              :src="data[index].banner_image"
+              class="w-full h-max bg-no-repeat bg-cover rounded-t"
+            />
+            <div class="m4">
+              <p class="text-sm color-gray mb4">{{ data[index].last_update }} | {{ data[index].news_category_title }}</p>
+              <p class="text-xl mb-0">{{ data[index].title }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div v-else class="flex grid grid-cols-3 gap-7 w-full mx-3 px-7 pr-4">
+        <div
+          v-for="(item, index) in 3"
+          :key="index"
+          class="carousel-card mx-5 h-full"
+        >
+          <div>
+            <img
+              :src="data[index].banner_image"
+              class="w-full h-max bg-no-repeat bg-cover rounded-t"
+            />
+            <div class="m4">
+              <p class="text-sm color-gray mb4">{{ data[index].last_update }} | {{ data[index].news_category_title }}</p>
+              <a :href="data[index].detail_url" class="text-xl mb-0">{{ data[index].title }}</a>
+            </div>
           </div>
         </div>
       </div>
@@ -32,18 +53,15 @@
 }
 
 .h-max {
-  max-height: 370px;
-}
-
-.carousel-img {
-  height: 262px;
-  transition: opacity 0.4s ease-in-out;
+  width: auto;
+  height: 230px;
 }
 
 </style>
 
 <script>
 export default {
-    
+  name: "NewsPortraitCard",
+  props: ["data", "isIndex"],
 }
 </script>
