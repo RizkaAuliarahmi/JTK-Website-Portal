42c49f53f3436594dac632882c1bfd9824c7cc3c
feuillelisse <nisashadrina@gmail.com>
2022-06-01 15:19:50 +0700

    Initialize Partnership view

---
+++ src/views/Partnership.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="partnership">
+        <Navbar/>
+        <BannerImage
+            title="Mitra Kami"
+            image="http://www.hope-a.com/images/food/agency-partnership.jpg"
+            color="text-white"
+        />
+        <div>
+            
+        </div>
+        <Footer/>
+    </div>
+</template>
+
+<style scoped>
+.h-150{
+    height: 150px;
+}
+</style>
+
+<script>
+import BannerImage from "../components/webjtk/BannerImage.vue";
+import Navbar from "../components/webjtk/Navbar.vue";
+import Footer from "../components/webjtk/Footer.vue";
+
+export default {
+    components: {
+        BannerImage, Navbar, Footer
+    },
+    name: "Partnership",
+}
+</script>
